53371152a97584e2447cc0d9a3fd1a309b1c3222
Yulia Cheliak <juliacheliak@gmail.com>
2021-03-15 23:53:48 +0200

    add initial web ui config

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'materialize-css';
+import { useRoutes } from './routes';
+
+const App = () => {
+  const routes = useRoutes(false);
+  return (
+    <Router>
+      <div className="container">
+        {routes}
+      </div>
+    </Router>
+  )
+};
+
+export default App;
